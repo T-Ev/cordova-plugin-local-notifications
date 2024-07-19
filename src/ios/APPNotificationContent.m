384c4695b1b717f8a751da55f8a5f9f8ef1aaebd
T-Ev <tjerobotics@gmail.com>
2024-07-19 17:11:21 -0400

    return to normal v1

--- src/ios/APPNotificationContent.m
+++ src/ios/APPNotificationContent.m
@@ -65,7 +65,7 @@ static char optionsKey;
     self.badge              = options.badge;
     self.attachments        = options.attachments;
     self.categoryIdentifier = options.actionGroupId;
-    // self.threadIdentifier = options.group;
+    self.threadIdentifier = options.group;
     // self.summaryArgument = options.summary;
  
 }
